ad750ed44850e3d8bdecbb556bf7bff1b9e255b3
Developer-mac <122273681+Developer-mac@users.noreply.github.com>
2023-01-12 07:46:28 +0300

    Add files via upload

---
+++ file.php
@@ -0,0 +1,35 @@
+<?php
+
+$name = $_POST['name']:
+$email = $_POST['email']:
+$message = $_POST['message']:
+
+$mailheaeder = "From:" .$name."<" .$email. ">\r\n";
+
+$recipient = "developermac24@gmail.com"
+
+mail($recipient, $subject, $message, $mailheaeder)
+or die("Error");
+
+echo '
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" type="text/css" href="STYLE1.CSS">
+  <form id="contact-form-id" class="contact-form-class" method="post" action="file.php">
+
+  <title>FEED BACK</title>
+</head>
+<body>
+  <section class="contact">
+   <h1>Thank you for contacting me.I will get back to you as soon as possible!</h1>
+   <p class="back">GO back to the <a href="index.html">Homepage</a>.</p>
+
+   </section>
+   <body>
+   </html>
+
+';
+?>
